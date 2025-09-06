f8f52dd729bb7d128d155e962efc642786fc7f5c
rosericazondekon <roseric_2000@yahoo.fr>
2025-09-06 14:49:36 -0400

    Add main C++ script

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "dataproc.h"
+#include <iostream>
+
+int main() {
+    DataHandler *dh = new DataHandler();
+    dh->loadFeatureVector("./train-images.idx3-ubyte");
+    dh->loadLabels("./train-labels.idx1-ubyte");
+    dh->countClasses();
+    dh->splitDataset();
+
+    // Example: Print out the first 5 labels from the dataset
+    auto dataset = dh->getDataset();
+    for (size_t i = 0; i < 5 && i < dataset->size(); i++) {
+        std::cout << "Label " << (int)(*dataset)[i]->getLabel() << " exists! Class = " << (*dataset)[i]->getEnumLabel() << std::endl;
+    }
+
+    return 0;
+}
